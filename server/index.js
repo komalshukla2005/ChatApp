69786b9ca9c0d4566a3d207de2f1c44baa062e1c
komalshukla2005 <komalshukla7518046264@gmail.com>
2025-05-09 14:53:19 +0530

    chatapp

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("send-message", (data) => {
+    io.emit("receive-message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+server.listen(3001, () => {
+  console.log(`Server running on http://localhost:3001`);
+});
